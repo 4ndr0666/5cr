e59701ea3e7bf25d0bce62ef05e64f30537efc15
4ndr0666 <97570105+4ndr0666@users.noreply.github.com>
2023-04-13 23:56:45 -0500

    Update process_video.sh

--- process_video.sh
+++ process_video.sh
@@ -1,28 +1,20 @@
 #!/bin/bash
+set -euo pipefail
 
-# process_video.sh
-# Usage: ./process_video.sh input_video.mp4 output_video.mp4
-
-input_video="$1"
-output_video="$2"
-temp_vpy=$(mktemp)
+readonly INPUT_VIDEO="$1"
+readonly OUTPUT_VIDEO="$2"
+readonly TEMP_VPY="$(mktemp).vpy"
 
 # Create a temporary VapourSynth script with the input video path
-cat <<EOF > "$temp_vpy"
-import vapoursynth as vs
-core = vs.get_core()
-clip = core.ffms2.Source("$input_video")
-EOF
-cat video_filter.vpy >> "$temp_vpy"
+printf "import sys\nsys.argv = [None, '%s']\n" "$INPUT_VIDEO" > "$TEMP_VPY"
+cat video_filter.vpy >> "$TEMP_VPY"
 
 # Debug: Print the content of the temporary VapourSynth script
-echo "=== Debug: Content of temporary VapourSynth script ==="
-cat "$temp_vpy"
-echo "======================================================="
+printf "=== Debug: Content of temporary VapourSynth script ===\n%s\n=======================================================\n" "$(cat "$TEMP_VPY")"
 
 # Process the video using the temporary VapourSynth script
 # Add frame rate conversion to 60fps using interpolation and remove the audio
-mpv "$input_video" --vf=vapoursynth="$temp_vpy",lavfi="[minterpolate='fps=60']" --o="$output_video" --no-audio --vo=gpu
+mpv "$INPUT_VIDEO" --vf=vapoursynth="$TEMP_VPY",lavfi="[minterpolate='fps=60']" --o="$OUTPUT_VIDEO" --no-audio --vo=gpu
 
 # Remove the temporary VapourSynth script
-rm "$temp_vpy"
+rm "$TEMP_VPY"
