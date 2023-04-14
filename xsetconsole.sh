f7c36a279be6eefe4e4bd7c86725a76a24d9f0a5
4ndr0666 <97570105+4ndr0666@users.noreply.github.com>
2023-04-13 23:58:45 -0500

    Update xsetconsole.sh

--- xsetconsole.sh
+++ xsetconsole.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
-#
-sudo cat <<EOF > /etc/vconsole.conf
+if sudo touch /etc/vconsole.conf &> /dev/null; then
+    if sudo cat <<EOF > /etc/vconsole.conf
 KEYMAP=us
-FONT=ter-v16b
+FONT=ter-220n
 EOF
+    then
+        echo "Successfully updated /etc/vconsole.conf"
+    else
+        echo "Failed to write to /etc/vconsole.conf"
+        exit 1
+    fi
+else
+    echo "Failed to obtain root privileges"
+    exit 1
+fi
