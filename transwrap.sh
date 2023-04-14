ecff13aefc48ba37102a2915cc24ec15be2199a6
4ndr0666 <97570105+4ndr0666@users.noreply.github.com>
2023-04-14 00:11:54 -0500

    Update transwrap.sh

--- transwrap.sh
+++ transwrap.sh
@@ -1,14 +1,18 @@
 #!/bin/zsh
-killall transmission-daemon 2> /dev/null
-transmission-daemon --foreground --log-info 2>&1 | while read line; do
-	echo $line |
-		grep -v "announcer.c:\|platform.c:\|announce done (tr-dht.c:" |
-		grep -v "Saved.*variant.c:" |
-		while read line; do
-			echo $line | grep -q "Queued for verification (verify.c:" &&
-				notify-send --app-name="Transmission Started" "${line#* * }"
-			echo $line | grep -q "changed from .Incomplete. to .Complete." &&
-				notify-send --app-name="Transmission Complete" "${line#* * }"
-			echo $line | systemd-cat --identifier="TransWrap" --priority=5
-		done 2>&1 > /dev/null
-	done&disown
+
+# Kill the transmission-daemon process
+killall transmission-daemon 2>/dev/null
+
+# Start transmission-daemon and filter its log output
+transmission-daemon --foreground --log-info 2>&1 | grep -v -e "announcer.c:" -e "platform.c:" -e "Saved.*variant.c:" | while read line; do
+    if echo "$line" | grep -q "Queued for verification (verify.c:"; then
+        # Notify when a download is queued for verification
+        notify-send --app-name="Transmission Started" "${line#* * }"
+    elif echo "$line" | grep -q "changed from .Incomplete. to .Complete."; then
+        # Notify when a download is complete
+        notify-send --app-name="Transmission Complete" "${line#* * }"
+    fi
+
+    # Log the filtered output using systemd-cat
+    echo "$line" | systemd-cat --identifier="TransWrap" --priority=5
+done & disown
