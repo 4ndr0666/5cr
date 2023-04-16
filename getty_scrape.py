ae488e52ba39caa91c1c988d78cd027ecb16d047
4ndr0666 <97570105+4ndr0666@users.noreply.github.com>
2023-04-16 08:40:18 -0500

    Update getty_scrape.py

--- getty_scrape.py
+++ getty_scrape.py
@@ -10,16 +10,10 @@ from urllib.parse import urljoin
 
 # Constants
 GETTY_IMAGES_URL = "https://www.gettyimages.com/"
-OUTPUT_DIR = "images"
 TIMEOUT = 30
 SCROLL_PAUSE_TIME = 2
 
-# Check if the output directory exists, if not, create it
-if not os.path.exists(OUTPUT_DIR):
-    os.makedirs(OUTPUT_DIR)
-
-def get_image_urls(driver, search_term):
-    search_url = urljoin(GETTY_IMAGES_URL, f'search/2/image?phrase={search_term}')
+def get_image_urls(driver, search_url, num_pages):
     driver.get(search_url)
 
     # Wait for the images to load
@@ -27,35 +21,55 @@ def get_image_urls(driver, search_term):
         EC.presence_of_element_located((By.CSS_SELECTOR, "img.gallery-asset__thumb"))
     )
 
-    # Scroll down to load more images
-    last_height = driver.execute_script("return document.body.scrollHeight")
-    while True:
-        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        time.sleep(SCROLL_PAUSE_TIME)
+    image_urls = []
+    
+    for _ in range(num_pages):
+        # Scroll down to load more images
+        last_height = driver.execute_script("return document.body.scrollHeight")
+        while True:
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            time.sleep(SCROLL_PAUSE_TIME)
 
-        new_height = driver.execute_script("return document.body.scrollHeight")
-        if new_height == last_height:
+            new_height = driver.execute_script("return document.body.scrollHeight")
+            if new_height == last_height:
+                break
+            last_height = new_height
+
+        # Get image URLs
+        image_elements = driver.find_elements_by_css_selector("img.gallery-asset__thumb")
+        image_urls += [img.get_attribute("src") for img in image_elements]
+
+        try:
+            next_page_button = driver.find_element_by_css_selector('.search-pagination__button-icon--next')
+            if next_page_button:
+                next_page_button.click()
+                time.sleep(2)
+            else:
+                break
+        except:
             break
-        last_height = new_height
-
-    # Get image URLs
-    image_elements = driver.find_elements_by_css_selector("img.gallery-asset__thumb")
-    image_urls = [img.get_attribute("src") for img in image_elements]
 
     return image_urls
 
-def download_images(image_urls, output_dir):
+def download_images(person_name, image_urls, output_dir):
     for idx, url in enumerate(image_urls):
         response = requests.get(url)
 
         if response.status_code == 200:
-            with open(os.path.join(output_dir, f'image_{idx}.jpg'), 'wb') as f:
+            with open(os.path.join(output_dir, f'{person_name}_{idx}.jpg'), 'wb') as f:
                 f.write(response.content)
         else:
             print(f"Failed to download image {idx}: {url}")
 
 def main():
-    search_term = input("Enter the search term: ")
+    person_name = input("Please provide the person's name: ")
+    search_url = input('Please provide the page URL: ')
+    output_dir = input('Please provide the directory where the data will be saved: ')
+    num_pages = int(input('Please provide how many pages you want to be scrapped: '))
+
+    # Check if the output directory exists, if not, create it
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
 
     options = webdriver.ChromeOptions()
     options.add_argument("--headless")
@@ -63,8 +77,8 @@ def main():
     options.add_argument("--no-sandbox")
 
     with webdriver.Chrome(options=options) as driver:
-        image_urls = get_image_urls(driver, search_term)
-        download_images(image_urls, OUTPUT_DIR)
+        image_urls = get_image_urls(driver, search_url, num_pages)
+        download_images(person_name, image_urls, output_dir)
 
 if __name__ == "__main__":
     main()
