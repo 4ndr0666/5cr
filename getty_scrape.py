e10d1968f856d49b21c2b45c5360beafdcdaf4ff
4ndr0666 <97570105+4ndr0666@users.noreply.github.com>
2023-04-16 08:11:50 -0500

    Create getty_scrape.py

---
+++ getty_scrape.py
@@ -0,0 +1,70 @@
+import os
+import time
+import requests
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from urllib.parse import urljoin
+
+# Constants
+GETTY_IMAGES_URL = "https://www.gettyimages.com/"
+OUTPUT_DIR = "images"
+TIMEOUT = 30
+SCROLL_PAUSE_TIME = 2
+
+# Check if the output directory exists, if not, create it
+if not os.path.exists(OUTPUT_DIR):
+    os.makedirs(OUTPUT_DIR)
+
+def get_image_urls(driver, search_term):
+    search_url = urljoin(GETTY_IMAGES_URL, f'search/2/image?phrase={search_term}')
+    driver.get(search_url)
+
+    # Wait for the images to load
+    WebDriverWait(driver, TIMEOUT).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, "img.gallery-asset__thumb"))
+    )
+
+    # Scroll down to load more images
+    last_height = driver.execute_script("return document.body.scrollHeight")
+    while True:
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(SCROLL_PAUSE_TIME)
+
+        new_height = driver.execute_script("return document.body.scrollHeight")
+        if new_height == last_height:
+            break
+        last_height = new_height
+
+    # Get image URLs
+    image_elements = driver.find_elements_by_css_selector("img.gallery-asset__thumb")
+    image_urls = [img.get_attribute("src") for img in image_elements]
+
+    return image_urls
+
+def download_images(image_urls, output_dir):
+    for idx, url in enumerate(image_urls):
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            with open(os.path.join(output_dir, f'image_{idx}.jpg'), 'wb') as f:
+                f.write(response.content)
+        else:
+            print(f"Failed to download image {idx}: {url}")
+
+def main():
+    search_term = input("Enter the search term: ")
+
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--no-sandbox")
+
+    with webdriver.Chrome(options=options) as driver:
+        image_urls = get_image_urls(driver, search_term)
+        download_images(image_urls, OUTPUT_DIR)
+
+if __name__ == "__main__":
+    main()
