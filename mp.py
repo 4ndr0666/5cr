873d575e920e5c6a1e7a8a2de484372d4300fb90
4ndr0666 <97570105+4ndr0666@users.noreply.github.com>
2023-04-22 08:02:18 -0500

    Update mp.py

--- mp.py
+++ mp.py
@@ -2,11 +2,7 @@
 
 import os
 import sys
-import toml
-from meta_package_manager import ManagerPool
-
-CONFIG_DIR = os.path.expanduser("~/.config/mpm")
-CONFIG_FILE = os.path.join(CONFIG_DIR, "packages.toml")
+import subprocess
 
 def print_menu():
     print("\nMenu:")
@@ -28,68 +24,44 @@ def print_menu():
     print("16. Run the doctor command in mpm")
     print("17. Exit")
 
-def save_to_config(data):
-    if not os.path.exists(CONFIG_DIR):
-        os.makedirs(CONFIG_DIR)
-    with open(CONFIG_FILE, "w") as f:
-        toml.dump(data, f)
-
-def load_config():
-    if os.path.exists(CONFIG_FILE):
-        with open(CONFIG_FILE, "r") as f:
-            return toml.load(f)
-    return {}
+def run_command(command):
+    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    output, error = process.communicate()
+    if output:
+        print(output.decode())
+    if error:
+        print(error.decode())
 
 def main():
-    pool = ManagerPool(allow_cli=True)
-    pool.load_managers()
-
     while True:
         print_menu()
         choice = int(input("\nEnter your choice: "))
         
         if choice == 1:
             package_name = input("Enter the package name to search: ")
-            search_package(pool, package_name)
+            run_command(["mpm", "search", package_name])
         elif choice == 2:
             package_name = input("Enter the package name to install: ")
-            install_package(pool, package_name)
+            run_command(["mpm", "install", package_name])
         elif choice == 3:
             package_name = input("Enter the package name to uninstall: ")
-            uninstall_package(pool, package_name)
+            run_command(["mpm", "remove", package_name])
         elif choice == 4:
-            list_installed_packages(pool)
+            run_command(["mpm", "list"])
         elif choice == 5:
-            list_available_updates(pool)
+            run_command(["mpm", "outdated"])
         elif choice == 6:
             package_name = input("Enter the package name to update: ")
-            update_package(pool, package_name)
+            run_command(["mpm", "update", package_name])
         elif choice == 7:
-            upgrade_all_packages(pool)
-        elif choice == 8:
-            print("This feature is not supported by the meta-package-manager.")
-        elif choice == 9:
-            upgrade_all_pip_packages(pool)
-        elif choice == 10:
-            package_name = input("Enter the package name to search exactly: ")
-            search_exact_package(pool, package_name)
-        elif choice == 11:
-            list_duplicate_installed_packages(pool)
-        elif choice == 12:
-            remove_duplicate_packages(pool)
-        elif choice == 13:
-            dump_installed_packages(pool)
-        elif choice == 14:
-            update_dumped_packages(pool)
-        elif choice == 15:
-            merge_latest_installed_packages(pool)
-        elif choice == 16:
-            run_doctor_command(pool)
-        elif choice == 17:
+            run_command(["mpm", "upgrade"])
+        # The rest of the cases require custom handling or are not supported by the meta-package-manager
+        else:
+            print("This feature is not supported by the meta-package-manager. Please choose an option from 1 to 7, or 17 to exit.")
+
+        if choice == 17:
             print("Exiting...")
             break
-        else:
-            print("Invalid choice. Please enter a valid number.")
 
 if __name__ == "__main__":
     main()
